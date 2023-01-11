c46737f3d669f81070a4a51af22cec788e1cfb46
BTNguyen9280 <briannguyen9280@yahoo.com>
2023-01-10 21:37:14 -0800

    add grid function and fill in on hover function

--- javascript.js
+++ javascript.js
@@ -1,2 +1,27 @@
-const div = document.createElement('div')
-div.style.color='blue'
+const grid = document.querySelector(".container")
+
+const makeDivs = (num, i=0) => {
+    var boxDimensions =960/num
+    if (i >=num*num) {
+        var elements = document.getElementsByClassName('cell');
+        Array.from(elements).forEach(function(element) {
+            element.style.height = boxDimensions + "px";
+            element.style.width = boxDimensions + "px";
+        })
+        return
+    }
+    let cells = document.createElement('div')
+    cells.classList.toggle('cell');
+    grid.appendChild(cells);
+    return makeDivs(num,++i);
+}
+
+makeDivs(8)
+
+var cells = document.getElementsByClassName('cell');
+Array.from(cells).forEach(function(cells) {
+    cells.addEventListener('mouseenter', (e) => {
+        cells.classList.add('filled')
+    })
+})
+
